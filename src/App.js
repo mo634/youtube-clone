2197e57d7cc2963dc212a9eb4138a98aea6d5a57
MohamedMostafa <mido221gyefue@gmail.com>
2024-01-18 12:07:42 +0200

    setup basic routes and basic components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import {Box} from "@mui/material";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const App = () => {
-return (
-<div>App</div>
-)
-}
+// import components
+import {Navbar , Feed, VideoDetail , ChannelDetail, SearchFeed} from './components';
 
-export default App
+
+const App = () => (
+    <BrowserRouter>
+        <Box>
+            
+        {/* render nav component  */}
+        <Navbar />
+        
+            {/* configure routes  */}
+        <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route path="/video/:id" element={<VideoDetail />} />
+            <Route path="/channel/:id" element={<ChannelDetail />} />
+            <Route path="/search/:searchTerm" element={<SearchFeed />} />
+            </Routes>
+            
+            
+        </Box>
+    </BrowserRouter>
+);
+
+export default App;
